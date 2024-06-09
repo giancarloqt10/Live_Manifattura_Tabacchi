40067b50f861c860ee863a81942609f0b1a0e3a6
Giancarlo Quin Tito <gian.f.quin@gmail.com>
2024-06-09 14:15:34 +0200

    mostrare i dati del form nella console e gestione dei click dei tag a nella navbar

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ let navbarHeight= navbar.clientHeight;
 
 let banNavHeight= bannerHeight + navbarHeight;
 
+// Aggiungi gestore eventi per il click sul logo
+document.querySelector('.logo a').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    const navbarHeight = document.querySelector('.navbar').offsetHeight;
+
+    // Scorri fluidamente all'elemento
+    smoothScroll(targetElement, navbarHeight);
+});
+
 // Gestione eventi
 navIcon.addEventListener('click', ()=>{
     navLinks.classList.toggle('active');
@@ -55,6 +67,7 @@ if(window.innerWidth < 1050){
     logoImg.src= "./media/logo_black.svg";
 }
 
+// Cambiare logo quando si scrolla
 window.addEventListener('scroll', ()=>{
     if(window.innerWidth < 1050){
         if(window.scrollY > banNavHeight){
@@ -73,7 +86,7 @@ window.addEventListener('scroll', ()=>{
     }
 })
 
-
+// ======= PROGETTO ======== //
 // SWIPER JS
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
@@ -102,6 +115,8 @@ let swiper = new Swiper(".mySwiper", {
     },
 });
 
+
+// ====== SOLUZIONI ABITATIVE ========
 // ANIMAZIONE SERVIZI
 let swiper2 = new Swiper(".mySwiper2", {
     navigation: {
@@ -124,3 +139,21 @@ let swiper2 = new Swiper(".mySwiper2", {
         }
     }
 });
+
+// ====== CONTATTI ======= //
+// Invia dati del form
+let form= document.getElementById('form');
+let submit= document.getElementById('submit');
+
+form.addEventListener('submit', function (event){
+    event.preventDefault();
+    let formData= new FormData(form);
+
+     // Iterare attraverso i dati del form
+    formData.forEach((value, key) => {
+        // Visualizzare i dati nella console
+        console.log(key + ': ' + value);
+    });
+    // Svuotare i campi del form
+    form.reset();
+})
